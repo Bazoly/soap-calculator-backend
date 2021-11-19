3869e37c98263cb41f748dbe84878863fa1d4518
Zoltán Baranyay <zbaranyay@gmail.com>
2021-11-19 18:06:38 +0100

    feat: implement subtraction() method in the Calculator class

--- SoapCalculator/src/main/java/com/bazoly/calculator/Calculator.java
+++ SoapCalculator/src/main/java/com/bazoly/calculator/Calculator.java
@@ -5,6 +5,9 @@ public class Calculator {
 	public int addition(int addend1, int addend2) {
 		return addend1 + addend2;
 	}
+	
+	public int subtraction(int minuend, int subtrahend) {
+		return minuend - subtrahend;
 	}
 
 }
